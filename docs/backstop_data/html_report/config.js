520a96f1467bf52d1b68a8cf5a27c04f07542584
Your Name <you@example.com>
2019-06-02 18:04:00 +0800

    提交所有文件

---
+++ docs/backstop_data/html_report/config.js
@@ -0,0 +1,42 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../../../backstop_data/bitmaps_reference/backstop_default_qqmap_0__0_phone.png",
+        "test": "../../../backstop_data/bitmaps_test/20190602-175624/backstop_default_qqmap_0__0_phone.png",
+        "selector": "",
+        "fileName": "backstop_default_qqmap_0__0_phone.png",
+        "label": "qqmap",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://map.qq.com/m/",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "engineErrorMsg": "Navigation failed because browser has disconnected!",
+        "error": "Reference file not found /Users/macdzw/Desktop/京城一灯学习笔记/Javascript与QA工程师/DZW-TEST/backstop_data/bitmaps_reference/backstop_default_qqmap_0__0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../../../backstop_data/bitmaps_reference/backstop_default_qqmap_0__1_tablet.png",
+        "test": "../../../backstop_data/bitmaps_test/20190602-175624/backstop_default_qqmap_0__1_tablet.png",
+        "selector": "",
+        "fileName": "backstop_default_qqmap_0__1_tablet.png",
+        "label": "qqmap",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://map.qq.com/m/",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "engineErrorMsg": "Navigation failed because browser has disconnected!",
+        "error": "Reference file not found /Users/macdzw/Desktop/京城一灯学习笔记/Javascript与QA工程师/DZW-TEST/backstop_data/bitmaps_reference/backstop_default_qqmap_0__1_tablet.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "backstop_default"
+});
---
+++ karma.conf.js
@@ -0,0 +1,66 @@
+// Karma configuration
+// Generated on Thu May 23 2019 00:36:30 GMT+0800 (GMT+08:00)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
+
+    // list of files / patterns to load in the browser
+    files: [
+      "./unit/**/*.js",
+      "./unit/**/*.spec.js"
+    ],
+
+    // list of files / patterns to exclude
+    exclude: [],
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      'unit/**/*.js': ['coverage']
+    },
+
+    //文档最终生成到哪里去
+    coverageReporter: {
+      type: 'html',
+      dir: 'docs/coverage/'
+    },
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress',
+      'coverage'
+    ],
+
+    // web server port
+    port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
